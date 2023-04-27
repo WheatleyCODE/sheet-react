15beabfb9aad71f66437e94d9f9e44f882974b62
WheatleyCODE <marmazy8@yandex.ru>
2023-04-27 13:31:38 +0300

    [FEAT] Add import types, add hook

---
+++ src/shared/lib/hooks/useClickOutsize.ts
@@ -0,0 +1,48 @@
+import { RefObject, useEffect } from 'react';
+
+export type EventTypes = 'click' | 'contextmenu';
+
+export const useClickOutside = <T extends HTMLElement = HTMLElement>(
+  ref: RefObject<T>,
+  handler: (event: MouseEvent) => void,
+  eventTypes: EventTypes[] = ['click'],
+  refOut?: RefObject<HTMLDivElement>
+) => {
+  useEffect(() => {
+    const fn = (event: MouseEvent) => {
+      const el = ref?.current;
+
+      if (!el || el.contains(event.target as Node)) {
+        return;
+      }
+
+      handler(event);
+    };
+
+    if (refOut) {
+      const cur = refOut.current;
+
+      if (!cur) return;
+
+      eventTypes.forEach((event) => {
+        cur.addEventListener(event, fn);
+      });
+
+      return () => {
+        eventTypes.forEach((event) => {
+          cur.addEventListener(event, fn);
+        });
+      };
+    }
+
+    eventTypes.forEach((event) => {
+      document.body.addEventListener(event, fn);
+    });
+
+    return () => {
+      eventTypes.forEach((event) => {
+        document.body.removeEventListener(event, fn);
+      });
+    };
+  }, [eventTypes, handler, ref, refOut]);
+};
--- src/shared/lib/kv-storage/index.ts
+++ src/shared/lib/kv-storage/index.ts
@@ -1,3 +1,6 @@
 export { LocalStorageEngine } from './local-storage-engine/localStorageEngine';
 export { SessionStorageEngine } from './session-storage-engine/sessionStorageEngine';
+export { IndexedDBEngine } from './indexed-db-engine/IndexedDBEngine';
+
 export { KVFactory, KVStorage } from './kv-storage';
+export type { SerializablePrimitiveValue, SerializableValue } from './interface';
