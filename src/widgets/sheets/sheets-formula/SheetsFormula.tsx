bd9b1e500e92187962e3829834a645d2a4332ff3
WheatleyCODE <marmazy8@yandex.ru>
2023-05-11 16:12:32 +0300

    [FEAT] Add formula template

--- src/widgets/sheets/sheets-formula/SheetsFormula.tsx
+++ src/widgets/sheets/sheets-formula/SheetsFormula.tsx
@@ -1,6 +1,20 @@
 import { FC } from 'react';
 import styles from './SheetsFormula.module.css';
+import { useTypedSelector } from 'shared/lib/hooks/redux/useTypedSelector';
+import { MdFunctions } from 'react-icons/md';
 
 export const SheetsFormula: FC = () => {
-  return <div className={styles.formula}></div>;
+  const { selectCells } = useTypedSelector((state) => state.table);
+
+  return (
+    <div className={styles.formula}>
+      <div className={styles.coords}>{selectCells[0].id}</div>
+      <div className={styles.textfild}>
+        <div className={styles.icon}>
+          <MdFunctions />
+        </div>
+        <input className={styles.input} type="text" />
+      </div>
+    </div>
+  );
 };
